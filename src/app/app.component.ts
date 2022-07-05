11a6678158abd668bb07ee6a171977add816c1a9
Sayed Haque <smhaque@outlook.com>
2022-07-04 19:48:47 -0700

    FirstCommit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { CurrencyService } from './service/currency.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  selectedCurrency : string = "USD";
+  constructor(private currencyService : CurrencyService){
+
+  }
+  sendCurrency(event:string){
+    console.log(event);
+    this.currencyService.setCurrency(event);
+  }
+}
